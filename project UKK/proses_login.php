b4347d054472ba91d2ded6958130ee0f493d5d5e
Daffa Shelby <98572099+DafaAF@users.noreply.github.com>
2023-02-03 15:30:56 +0700

    Add files via upload

---
+++ project UKK/proses_login.php
@@ -0,0 +1,18 @@
+<?php
+    include("koneksi.php");
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $query = $db->query("select * from petugas where username='$username' AND password='$password'"); // jalankan query
+    $dataArray = $query->fetchAll(); // ubah ke bentuk array
+
+    if($dataArray){
+        $_SESSION['username'] = $username;
+        $_SESSION['password'] = $password;
+        header("Location:./Admin/Tampilan.php");
+    }
+    else{
+        header("Location:login.php");
+    }
+?>
